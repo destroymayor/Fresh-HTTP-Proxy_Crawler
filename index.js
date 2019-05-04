4a142112bd46c4695adf380a275a2aab9da45287
Jared <kuil5230@gmail.com>
2019-05-04 20:03:36 +0800

    .

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ import cheerio from "cheerio";
 const superagent = require("superagent");
 require("superagent-proxy")(superagent);
 
-const getProxyUrl = async url => {
+const getProxyUrl1 = async (url, tableTr, tdChild_IPHttp, tdChild_URL, tdChild_Port) => {
   const IP_proxy_RequestList = [];
   try {
     const RequestHTML = await superagent.get(url);
     const $ = cheerio.load(RequestHTML.text);
-    $(`#page > table.bg > tbody > tr`).map((index, value) => {
+    $(tableTr).map((index, value) => {
       const IP_http =
         $(value)
-          .children("td:nth-child(7)")
+          .children(tdChild_IPHttp)
           .text() === "no"
           ? "http"
           : "https";
       const IP_url = $(value)
-        .children("td:nth-child(2)")
+        .children(tdChild_URL)
         .text();
       const IP_port = $(value)
-        .children("td:nth-child(3)")
+        .children(tdChild_Port)
         .text();
       IP_proxy_RequestList.push(`${IP_http}://${IP_url}:${IP_port}`);
     });
@@ -41,6 +41,21 @@ const getProxyUrl = async url => {
   }
 };
 
+//https://list.proxylistplus.com/Fresh-HTTP-Proxy-List-1
 for (let i = 0; i <= 6; i++) {
-  getProxyUrl("https://list.proxylistplus.com/Fresh-HTTP-Proxy-List-" + i);
+  getProxyUrl1(
+    `https://list.proxylistplus.com/Fresh-HTTP-Proxy-List-${i}`,
+    "#page > table.bg > tbody > tr",
+    "td:nth-child(7)",
+    "td:nth-child(2)",
+    "td:nth-child(3)"
+  );
 }
+
+// getProxyUrl1(
+//   "https://free-proxy-list.net/",
+//   "#proxylisttable > tbody > tr",
+//   "td:nth-child(7)",
+//   "td:nth-child(1)",
+//   "td:nth-child(2)"
+// );
